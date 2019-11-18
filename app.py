96c4bdb648550ba40670953529b944528fa392fc
mrbrunotte <mrbrunotte@gmail.com>
2019-11-18 22:35:49 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,134 @@
+#----------------#
+# DEPENDENCIES
+#----------------#
+import os
+import pymongo
+import math  # remove?
+import random  # remove?
+from flask import Flask, render_template, redirect, request, url_for, request, session, g, abort, flash
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from datetime import datetime, timedelta
+from werkzeug.security import generate_password_hash, check_password_hash
+
+#-----------------------------#
+# CONNECT TO MONGODB DATABASE
+#-----------------------------#
+
+app = Flask(__name__)
+app.config['MONGO_DBNAME'] = 'foodictionary'
+# app.config['MONGO_URI'] = os.getenv('MONGO_URI', 'mongodb://localhost')
+app.config['MONGO_URI'] = 'mongodb+srv://mrbrunotte:mrUSERbrunotte@foodictionary-gckbp.mongodb.net/foodictionary?retryWrites=true&w=majority'
+app.secret_key = '9893869affbf35907d0e7f0f20a72bc9'
+# Add secret key:   app.secret_key = os.getenv('SECRET', 'randomstring123')
+
+mongo = PyMongo(app)
+
+#-----------#
+# VARIABLES
+#-----------#
+
+userDB = mongo.db.users
+
+
+#----------#
+# HOMEPAGE
+#----------#
+
+# base.html and root route
+@app.route('/')
+@app.route('/home')
+def home():
+    # returns everything from tasks in task_manager DB
+    return render_template('base.html')
+
+#-----------------------------#
+# USER LOGIN AND REGISTRATION
+#-----------------------------#
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=30)
+    author_name = request.form.get('author_name')
+    username = request.form.get('username').lower()
+    password = generate_password_hash(request.form.get('password'))
+    session['username'] = username
+    user = userDB.find_one({'username': username})
+
+    if user is None:
+        userDB.insert_one({
+            'author_name': author_name,
+            'username': username,
+            'password': password
+        })
+        session['logged_in'] = True
+        flash('Welcome to FOODictionary ' + author_name)
+        return login()
+    else:
+        session['logged_in'] = False
+        flash('Username already exists, please try again.')
+    return register()
+
+
+@app.route('/login')
+def login():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template('base.html')
+
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    username = request.form['username'].lower()
+    password = request.form['password']
+    session['username'] = username
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=30)
+    user = userDB.find_one({'username': username})
+
+    if not user:
+        session['logged_in'] = False
+        flash('User ' + session['username'] +
+              ' is not registered. Please try again.')
+        return signin()
+    elif not check_password_hash(user['password'], password):
+        session['logged_in'] = False
+        flash('Wrong Password, please try again.')
+        return signin()
+    else:
+        session['logged_in'] = True
+        flash('Welcome back ' + user['author_name'].capitalize())
+        return render_template('base.html')
+
+
+@app.route('/logout')
+def logout():
+    session['logged_in'] = False
+    return render_template('base.html', page_title='Logout from FOODictionary')
+
+#-------------#
+# Error Pages
+#-------------#
+
+# @app.errorhandler(404)
+# def page_not_found(error):
+#    return render_template('404.html',recipeCategory=recipeCategory.find(), page=1, page_title='404 Error Page, FOODictionary'), 404
+
+
+# @app.errorhandler(500)
+# def something_wrong(error):
+    # return render_template('500.html',recipeCategory=recipeCategory.find(), page=1, page_title='500 Error Page, FOODictionary'), 500
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=(os.environ.get('PORT')),
+            debug=True)  # change to False when finished!
---
+++ static/script.js
@@ -0,0 +1,92 @@
+/* random meal API script */
+const get_meal_btn = document.getElementById('get_meal');
+const meal_container = document.getElementById('meal');
+
+/* Event listener for the click event, the function makes a request to the API */
+get_meal_btn.addEventListener('click', () => {
+	fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+		.then(res => res.json())
+		.then(res => {
+			createMeal(res.meals[0]);
+		})
+		.catch(e => {
+			console.warn(e);
+		});
+});
+
+
+const createMeal = meal => {
+	const ingredients = [];
+
+	// Get all ingredients from the object. Up to 20
+	for (let i = 1; i <= 20; i++) {
+		if (meal[`strIngredient${i}`]) {
+			ingredients.push(
+				`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+			);
+		} else {
+			// Stop if there are no more ingredients
+			break;
+		}
+	}
+
+	const newInnerHTML = `
+			<h2 class="random-meal-name"><p>Your random dish is called:</p>"${meal.strMeal}"</h2>
+			
+			<div class="img-header-parent">
+				<h2 class="image-header">${meal.strMeal}</h2>
+			</div>
+
+			<div class="tags">
+				${meal.strCategory
+			? `<p class="meal-info"><strong>Category:</strong> ${meal.strCategory}</p>`
+			: ''
+		}
+						${meal.strArea ? `<p class="meal-info"><strong>Area:</strong> ${meal.strArea}</p>` : ''}
+						${
+		meal.strTags
+			? `<p class="meal-info"><strong>Tags:</strong> ${meal.strTags
+				.split(',')
+				.join(', ')}</p>`
+			: ''
+		}
+			</div>
+
+			<div class="tags-parent">
+			<h2 class="tags-header">tags:</h2>
+			</div>
+			<div class="img-food-parent">
+				<img class="img-food" src="${meal.strMealThumb}" alt="Meal Image">
+			</div>
+			<div class="ingr-parent">
+				<h2 class="ingr-header">Ingredients:</h2>
+			</div>
+				<div class="ingredients">
+					<ul>
+						${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+					</ul>
+			</div>
+		
+			<div class="meal-instr-parent">
+				<h2 class="meal-instr-header">Cooking instructions:</h2>
+			</div>
+				<div class="instructions">
+				<p class="meal-instr">${meal.strInstructions}</p>
+			</div>
+		</div>
+		${
+		meal.strYoutube
+			? `
+		<div class="video-parent">
+		<div class="videoWrapper">
+			<iframe width="320" height="215"
+				src="https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}" alt="No video available">
+			</iframe>
+		</div>
+		</div>`
+			: ''
+		}
+	`;
+
+	meal_container.innerHTML = newInnerHTML;
+};
